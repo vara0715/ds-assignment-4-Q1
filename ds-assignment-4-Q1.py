1f328e7ad02f875303e6a3acd7ee67b37dac909e
vara0715 <88980746+vara0715@users.noreply.github.com>
2022-12-04 23:14:42 +0530

    Update ds-assignment-4-Q1.py

--- ds-assignment-4-Q1.py
+++ ds-assignment-4-Q1.py
@@ -1,5 +1,2 @@
-Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-= RESTART: C:/Users/varap/AppData/Local/Programs/Python/Python311/ds-assignment-4-q1.py
-35
+r = lambda a : a + 25
+print(r(10))
